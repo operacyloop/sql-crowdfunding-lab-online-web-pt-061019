b0a8e4064123f47a35a385591c9255f761ac6cb4
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-08-16 00:12:59 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   ON pledges.project_id = projects.id 
   WHERE category ='music';"
   
-  -- "SELECT projects.category, pledges.amount FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'music';"
+  "SELECT projects.category, pledges.amount FROM projects JOIN pledges -- ON projects.id = pledges.project_id WHERE projects.category = 'music';"
   
 end
 
